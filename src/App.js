f56907f88762a1d0b62ccfabcaa2c23f12ca6735
Mariagoretti <mariakimanigoreti@gmail.com>
2023-07-06 06:12:15 +0300

    fetch API

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Login';
+import Products from './Products';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
